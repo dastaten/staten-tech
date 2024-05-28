8bd0d1093b699215e0119661ba7b21505f1be475
Dorian Staten <dastaten@gmail.com>
2024-05-27 20:38:21 -0500

    Hamburger menu done, needs tweaking.

--- src/components/CreateNavLink.jsx
+++ src/components/CreateNavLink.jsx
@@ -1,19 +1,42 @@
 import { NavLink } from 'react-router-dom';
 import { useEffect } from 'react';
 
-export default function CreateNavLink({ children, title, path, pageTitle, setPageTitle }) {
+export default function CreateNavLink({
+  children,
+  title,
+  path,
+  pageTitle,
+  setPageTitle,
+  handleToggleMenu,
+  type,
+}) {
 
   useEffect(() => {
     document.title = `${pageTitle} | Dorian Staten's Front End Developer Portfolio`;
   }, [pageTitle]);
 
+  if (type === 'hamburger') {
+    return (
+      <NavLink
+        className='p-2 rounded-md text-[#a1ceff]'
+        onClick={() => {
+          handleToggleMenu();
+          setPageTitle(title);
+        }}
+        to={path}
+      >
+        {children}
+      </NavLink>
+    );
+  }
+
   return (
     <NavLink
       className={({ isActive }) =>
-        `transition-all duration-300 text-[#a1ceff] p-2 rounded-md ${isActive
-          ? 'bg-[#a1ceff] text-[#38435c]'
-          : 'hover:bg-[#a1ceff] hover:text-[#38435c]'
-        }`
+        `transition-all duration-300 ${isActive
+          ? 'text-[#38435c] bg-[#a1ceff]'
+          : 'text-[#a1ceff] hover:bg-[#a1ceff] hover:text-[#38435c]'
+        } p-2 rounded-md`
       }
       to={path}
       onClick={() => setPageTitle(title)}
@@ -22,3 +45,4 @@ export default function CreateNavLink({ children, title, path, pageTitle, setPag
     </NavLink>
   );
 }
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,160 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 import CreateNavLink from './CreateNavLink';
 
 export default function Navbar({ error }) {
   const [pageTitle, setPageTitle] = useState('Welcome');
+  const [menuActive, setMenuActive] = useState(false);
+  const menuRef = useRef(null);
+  const hamburgerRef = useRef(null);
+
+  const handleToggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        !hamburgerRef.current.contains(event.target)
+      ) {
+        setMenuActive(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
-    <nav className='flex items-center justify-between py-5 bg-[#1f2533] px-7 text-default'>
-      <div className='text-2xl font-bold md:text-3xl lg:text-4xl'>{error || pageTitle}</div>
-      <ul className='hidden space-x-3 text-lg font-semibold sm:text-base md:text-lg lg:text-xl sm:flex'>
-        <li>
-          <CreateNavLink
-            title="Welcome"
-            path="/"
-            pageTitle={pageTitle}
-            setPageTitle={setPageTitle}>
-            Home
-          </CreateNavLink>
-        </li>
-        <li>
-          <CreateNavLink
-            title="About me"
-            path="/about"
-            pageTitle={pageTitle}
-            setPageTitle={setPageTitle}>
-            About
-          </CreateNavLink>
-        </li>
-        <li>
-          <CreateNavLink
-            title="My skills"
-            path="/skills"
-            pageTitle={pageTitle}
-            setPageTitle={setPageTitle}>
-            Skills
-          </CreateNavLink>
-        </li>
-        <li>
-          <CreateNavLink
-            title="My projects"
-            path="/projects"
-            pageTitle={pageTitle}
-            setPageTitle={setPageTitle}>
-            Projects
-          </CreateNavLink>
-        </li>
-        <li>
-          <CreateNavLink
-            title="Contact me"
-            path="/contact"
-            pageTitle={pageTitle}
-            setPageTitle={setPageTitle}>
-            Contact
-          </CreateNavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className='flex items-center justify-between py-5 bg-[#1f2533] px-7 text-default'>
+        <div className='text-2xl font-bold md:text-3xl lg:text-4xl'>{error || pageTitle}</div>
+        <ul className='hidden space-x-3 text-lg font-semibold sm:text-base md:text-lg lg:text-xl sm:flex'>
+          <li>
+            <CreateNavLink
+              title="Welcome"
+              path="/"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}>
+              Home
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="About me"
+              path="/about"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}>
+              About
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="My skills"
+              path="/skills"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}>
+              Skills
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="My projects"
+              path="/projects"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}>
+              Projects
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="Contact me"
+              path="/contact"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}>
+              Contact
+            </CreateNavLink>
+          </li>
+        </ul>
+        <div
+          className='m-0 overflow-visible transition-opacity duration-150 ease-linear bg-transparent border-0 cursor-pointer transition-filter font-inherit text-inherit sm:hidden'
+          ref={hamburgerRef}
+          onClick={handleToggleMenu}>
+          <div>
+            <span className={`${menuActive ? 'translate-y-[7px] rotate-45' : ''} bar block w-[25px] h-[2px] my-[5px] mx-auto transition duration-300 ease-in-out bg-[#a1ceff]`}></span>
+            <span className={`${menuActive ? 'opacity-0' : ''} bar block w-[25px] h-[2px] my-[5px] mx-auto transition duration-300 ease-in-out bg-[#a1ceff]`}></span>
+            <span className={`${menuActive ? '-translate-y-[7px] -rotate-45' : ''} bar block w-[25px] h-[2px] my-[5px] mx-auto transition duration-300 ease-in-out bg-[#a1ceff]`}></span>
+          </div>
+        </div>
+      </nav>
+      <nav
+        className={`rounded-b-lg absolute right-0 z-10 sm:hidden opacity-0 transition-opacity duration-200 ease-in-out bg-[#1f2533] ${menuActive ? 'active opacity-100' : ''}`}
+        ref={menuRef}>
+        <ul className='flex flex-col items-center gap-5 text-xl p-7'>
+          <li>
+            <CreateNavLink
+              title="Welcome"
+              path="/"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}
+              handleToggleMenu={handleToggleMenu}
+              type='hamburger'>
+              Home
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="About me"
+              path="/about"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}
+              handleToggleMenu={handleToggleMenu}
+              type='hamburger'>
+              About
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="My skills"
+              path="/skills"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}
+              handleToggleMenu={handleToggleMenu}
+              type='hamburger'>
+              Skills
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="My projects"
+              path="/projects"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}
+              handleToggleMenu={handleToggleMenu}
+              type='hamburger'>
+              Projects
+            </CreateNavLink>
+          </li>
+          <li>
+            <CreateNavLink
+              title="Contact me"
+              path="/contact"
+              pageTitle={pageTitle}
+              setPageTitle={setPageTitle}
+              handleToggleMenu={handleToggleMenu}
+              type='hamburger'>
+              Contact
+            </CreateNavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
