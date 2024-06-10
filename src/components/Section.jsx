1141e055d0e7262d066eb7be47b66be9d48d7de4
Dorian Staten <dastaten@gmail.com>
2024-06-10 18:09:14 -0500

    Back to VH.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,7 +3,7 @@ import Particle from './Particle';
 export default function Section({ children }) {
   return (
     <section>
-      <div className='p-10 text-default flex items-center justify-center w-full min-h-[93dvh] md:min-h-[92dvh] lg:min-h-[91dvh]'>
+      <div className='p-10 text-default flex items-center justify-center w-full min-h-[93vh] md:min-h-[92vh] lg:min-h-[91vh]'>
         {children}
         <Particle />
       </div>
