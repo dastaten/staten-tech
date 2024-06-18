cc0f72fb142c58d80b483cc2a0fe52eb517184b0
Dorian Staten <dastaten@gmail.com>
2024-06-18 16:41:11 -0500

    Adjusted responsiveness.

--- src/projects/flex-box/StyleDisplay.jsx
+++ src/projects/flex-box/StyleDisplay.jsx
@@ -17,8 +17,8 @@ export default function StyleDisplay({ currentStyles, numberOfBoxes, handleSelec
   };
 
   return (
-    <div className='flex w-full gap-3'>
-      <div className='flex border-2 rounded-md border-sky-700 min-h-[5vh] w-fit min-w-[130px] text-sky-200 justify-center items-center text-lg lg:text-xl font-bold pt-1 px-3'>
+    <div className='flex flex-col w-full gap-3 md:flex-row'>
+      <div className='flex border-2 rounded-md border-sky-700 min-h-[5vh] w-full md:w-fit min-w-[130px] text-sky-200 justify-center items-center text-lg lg:text-xl font-bold pt-1 px-3'>
         <span>
           Boxes:
           <select
@@ -46,9 +46,9 @@ export default function StyleDisplay({ currentStyles, numberOfBoxes, handleSelec
       </div>
       <div className='flex border-2 rounded-md border-sky-700 min-h-[5vh] grow text-sky-200 justify-center items-center text-lg lg:text-xl font-bold pt-1'>
         {currentStyles.includes('flex') ? (
-          <div className='flex flex-wrap items-center gap-2'>
+          <div className='flex flex-wrap items-center justify-center gap-2'>
             <span className='whitespace-nowrap'>Current styles:</span>
-            <span className='text-sky-300'>{currentStyles.join(' ')}</span>
+            <span className='text-center text-sky-300'>{currentStyles.join(' ')}</span>
             <div>
               <img
                 src={copy}
