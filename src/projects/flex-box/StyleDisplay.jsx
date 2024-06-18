c39876d816d1177b3be144243c4fed03af83e7d1
Dorian Staten <dastaten@gmail.com>
2024-06-18 15:56:55 -0500

    Adjusted copy.png and centered it vertically.

--- src/projects/flex-box/StyleDisplay.jsx
+++ src/projects/flex-box/StyleDisplay.jsx
@@ -31,7 +31,7 @@ export default function StyleDisplay({ currentStyles, numberOfBoxes, handleSelec
       </div>
       <div className='flex border-2 rounded-md border-sky-700 min-h-[5vh] grow text-sky-200 justify-center items-center text-lg lg:text-xl font-bold pt-1'>
         {currentStyles.includes('flex') ? (
-          <div className='flex flex-wrap'>
+          <div className='flex flex-wrap items-center'>
             <span className='whitespace-nowrap'>Current styles:&nbsp;</span>
             <span className='text-sky-300'>{currentStyles.join(' ')}</span>
             {<img src={copy} alt='copy' className='w-5 h-5 ml-3 cursor-pointer' onClick={() => navigator.clipboard.writeText(currentStyles.join(' '))} />}
